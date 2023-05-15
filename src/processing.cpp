e3693348b94c08abfb446e67d422195951d023c1
ssh4 <libalias@gmail.com>
2023-05-15 19:24:09 +0900

    splitting code

--- src/processing.cpp
+++ src/processing.cpp
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QLabel>
 #include <QtGui/QDragEnterEvent>
@@ -23,46 +22,30 @@
 #include <QtCore/QDebug>
 #include <QtCore/QFileInfo>
 
-#include "processing.h"
+#include "solidify.h"
 
-class DropArea : public QLabel {
-public:
-    DropArea() {
-        setAcceptDrops(true);
-        resize(400, 400);
-        setText("Drag-n-drop files here"); // Set text
-        setAlignment(Qt::AlignCenter); // Set alignment to center
-    }
+std::vector<QString> fileNames; // This will hold the names of all files
 
-protected:
-    void dragEnterEvent(QDragEnterEvent* event) override {
-        event->acceptProposedAction();
-    }
-
-    void dropEvent(QDropEvent* event) override {
-        QList<QUrl> urls = event->mimeData()->urls();
-        if (urls.isEmpty()) {
-            return;
+bool doProcessing(QList<QUrl> urls) {
+    for (const QUrl& url : urls) {
+        QString fileName = url.toLocalFile();
+        if (!fileName.isEmpty()) {
+            fileNames.push_back(fileName);
         }
-
-        //QString fileName = urls.first().toLocalFile();
-        //if (fileName.isEmpty()) {
-        //    return;
-        //}
-
-        // Processing
-
-        bool success = doProcessing(urls);
-
-        qDebug() << "Done!";
     }
-};
 
-int main(int argc, char* argv[]) {
-    QApplication app(argc, argv);
+    for (int i = 0; i < fileNames.size(); i++) {
+        //qDebug() << "File name: " << fileName;
+        QFileInfo fileInfo(fileNames[i]);
+        QString baseName = fileInfo.baseName();
+        QString path = fileInfo.absolutePath();
+        QString outName = path + "/" + baseName + "_fill." + fileInfo.completeSuffix();
 
-    DropArea dropArea;
-    dropArea.show();
+        // Call the solidify_main function
+        if (solidify_main(fileNames[i].toStdString(), outName.toStdString())) {
+            exit(-1);
+        };
+    }
 
-    return app.exec();
+    return true;
 }
